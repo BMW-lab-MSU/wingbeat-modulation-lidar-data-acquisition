9fdea50cec60f5442b00e92f02d90f1a6df7a0c7
Trevor Vannoy <trevorvannoy@montana.edu>
2023-09-26 11:26:42 -0600

    add config file parsing

--- digitizer.py
+++ digitizer.py
@@ -1,16 +1,17 @@
 import warnings
+import tomllib
+from typing import NamedTuple
 
 import PyGage
-
-# I don't think we need to put these imports under a namespace.
-# The chances of us creating a variable with the same name is very small.
-# from GageConstants import *
+from GageConstants import CS_MODE_SINGLE,CS_TRIG_COND_POS_SLOPE,CS_TRIG_COND_NEG_SLOPE,CS_TRIG_SOURCE_EXT
 
 class Digitizer:
-    def __init__(self,config_file=None):
+    def __init__(self,config_filename=None):
         self._digitizer_handle = None
         self.system_info = None
-        self.config = None
+        self.acquistion_config = None
+        self.channel_config = None
+        self.trigger_config = None
 
     def __del__(self):
         # Explicitly free the digitizer before the instance is about to be
@@ -20,7 +21,7 @@ class Digitizer:
         self.free()
 
     def initialize(self):
-        """Initialize the digitizer"""
+        """Initializes the digitizer"""
         status = PyGage.Initialize()
         if status < 0:
             raise RuntimeError(f"Failed to initialize digitizer.\nErrno = {status}, {PyGage.GetErrorString(status)}")
@@ -43,8 +44,67 @@ class Digitizer:
             raise RuntimeError(f"Failed to get digitizer info.\nErrno = {self.system_info}, {PyGage.GetErrorString(self.system_info)}")
         
 
-    def load_configuration(self,config_file):
-        pass
+    def load_configuration(self,filename):
+        """Loads a digitizer configuration from a TOML file
+
+        Parses a TOML configuration and puts the configuration values
+        into the aquisition_config, trigger_config, and channel_config
+        NamedTuples.
+
+        Args:
+            filename (str): Config filename
+        """
+
+        # Parse toml file
+        with open(filename,'rb') as f:
+            config = tomllib.load(f)
+
+        self.acquisition_config = AcquisitionConfig(
+            SampleRate=int(config['acquisition']['sample_rate']),
+            Depth=config['acquisition']['n_samples'],
+            SegmentSize=config['acquisition']['n_samples'],
+            TriggerDelay=config['acquisition']['trigger_delay'],
+            SegmentCount=config['acquisition']['segment_count'],
+        )
+
+        self.channel_config = ChannelConfig(
+            Channel=config['channel']['channel'],
+            InputRange=config['channel']['range'],
+            DcOffset=config['channel']['dc_offset']
+        )
+
+        # Parse trigger source
+        if isinstance(config['trigger']['source'],str):
+            if config['trigger']['source'].lower() == 'external':
+                trigger_source = CS_TRIG_SOURCE_EXT
+            else:
+                raise ValueError("Invalid trigger source setting.\n"
+                    + "Must be one of {\"external\",1,2}.")
+        else:
+            trigger_source = config['trigger']['source']
+
+        # Parse the trigger condition. The trigger condition can be any of:
+        # rising/r/positive/p/1 for positive-edge triggered, and
+        # falling/f/negative/n/0 for negative-edge triggered
+        if isinstance(config['trigger']['condition'],str):
+            condition = config['trigger']['condition'].lower()
+            if condition in {'rising','r','positive','p'}:
+                condition = CS_TRIG_COND_POS_SLOPE
+            elif condition in {'falling','f','negative','n'}:
+                condition = CS_TRIG_COND_NEG_SLOPE
+            else:
+                raise ValueError("Invalid trigger condition setting.\n"
+                    + "Must be one of {\"rising\", \"r\", \"positive\", \"p\", 1, \"falling\", \"f\", \"negative\", \"n\", 0}.")
+        else:
+            condition = config['trigger']['condition']
+
+        self.trigger_config = TriggerConfig(
+            Condition=condition,
+            Level=config['trigger']['level'],
+            Range=config['trigger']['range'],
+            Source=trigger_source
+        )
+
 
     def configure(self):
         pass
@@ -59,7 +119,7 @@ class Digitizer:
         pass
 
     def free(self):
-        """Free the digitizer so other applications can use it."""
+        """Frees the digitizer so other applications can use it."""
         # Only free the system if we have a reference to it.
         if self._digitizer_handle:
             status = PyGage.FreeSystem(self._digitizer_handle)
@@ -71,3 +131,77 @@ class Digitizer:
                 self._digitizer_handle = None
         else:
             warnings.warn("Not attempting to free system. We don't have a reference to any digitizer.",RuntimeWarning)
+
+
+# NOTE: we do not support pre-trigger data, forced-trigger timeouts, non-default timestamp modes,
+# dual-channel acquisition, or external clocking 
+class AcquisitionConfig(NamedTuple):
+    """Acquisition configuration values
+
+    Attributes:
+        SampleRate (int): 
+            Digitizer sampling rate
+        SegmentCount (int):
+            Number of segments to acquire. This is the number of trigger
+            events to acquire.
+        Depth (int):
+            Number of samples to acquire for each trigger event
+        SegmentSize (int): 
+            Number of samples to acquire for each trigger event. Should be
+            the same as Depth since we don't support acquiring
+            pre-trigger data. 
+        TriggerDelay (int):
+            Number of samples between the trigger event and the start of
+            data acquisition.
+        Mode (int):
+            Acquisition mode. We only support single-channel acquisition,
+            so this defaults to single-acquisition mode.
+
+    Notes:
+        Both Depth and SegmentSize are present because the digitizer needs both
+        of those configuration parameters, even though we want them to be the same. 
+    """
+    # TODO: we could remove Depth or SegmentSize, then add it back to the dictionary
+    # before we send the configuration to the driver. This would enforce the user to
+    # make the Depth and SegmentSize the same. We could do the same thing with Mode.
+    SampleRate: int
+    SegmentCount: int
+    Depth: int
+    SegmentSize: int
+    TriggerDelay: int
+    Mode: int = CS_MODE_SINGLE
+
+class ChannelConfig(NamedTuple):
+    """Channel configuration values
+
+    Attributes:
+        Channel (int):
+            Channel number
+        InputRange (int):
+            Input voltage range
+        DcOffset (int):
+            DC offset for the channel. This can span the full scale
+            of the input voltage range, i.e. +/-(InputRange/2)
+    """
+    Channel: int
+    InputRange: int
+    DcOffset: int
+
+# NOTE: we are only going to support "simple" triggering mode using 1 trigger engine
+class TriggerConfig(NamedTuple):
+    """Trigger configuration values
+
+    Attributes:
+        Condition (int):
+            Trigger condition. 0 for negative-edge and 1 for positive-edge
+        Level (int):
+            Trigger level as a percentage of the input voltage range
+        Range (int):
+            Input voltage range
+        Source (int):
+            Trigger source. See GageConstants.py for valid values.
+    """
+    Condition: int
+    Level: int
+    Range: int
+    Source: int
---
+++ tests/test_digitizer.py
@@ -0,0 +1,124 @@
+import unittest
+from unittest.mock import Mock, MagicMock, patch
+
+# import PyGage
+
+from GageConstants import *
+from digitizer import *
+
+class TestDigitizer(unittest.TestCase):
+
+    def setUp(self):
+        self.digitizer = Digitizer()
+        # self.mock = MagicMock(spec=PyGage)
+    
+
+    def test_free_no_handle(self):
+        with self.assertWarnsRegex(RuntimeWarning,"Not attempting to free system. We don't have a reference to any digitizer.") as cm:
+            self.digitizer.free()
+        
+
+    @patch('PyGage.FreeSystem')
+    def test_free_valid_handle(self,mock):
+        mock.return_value = 1
+
+        self.digitizer._digitizer_handle = 1
+
+        self.assertEqual(self.digitizer._digitizer_handle,1)
+
+        self.digitizer.free()
+
+        self.assertEqual(self.digitizer._digitizer_handle,None)
+
+    # @patch('PyGage.FreeSystem')
+    # def test_free_invalid_handle(self,mock):
+    #     # TODO: import GageErrors so we don't have magic numbers
+    #     mock.return_value = -6 # invalid handle
+
+    #     # mock a valid intialization
+    #     self.digitizer._digitizer_handle = 2
+
+    #     self.assertEqual(self.digitizer._digitizer_handle,2)
+
+    #     with self.assertWarnsRegex(RuntimeWarning,"Failed to free system"):
+    #         self.digitizer.free()
+
+
+    def test_valid_config_file1(self):
+        config_filename = 'tests/example-config-1.toml'
+
+        self.digitizer.load_configuration(config_filename)
+
+        # Create expected config tuples
+        expected_acq_config = AcquisitionConfig(
+            SampleRate=int(1e9),
+            Mode=CS_MODE_SINGLE,
+            SegmentCount=8,
+            SegmentSize=65536,
+            Depth=65536,
+            TriggerDelay=0
+        )
+
+        expected_trig_config = TriggerConfig(
+            Condition=CS_TRIG_COND_POS_SLOPE,
+            Level=50,
+            Range=3000,
+            Source=CS_TRIG_SOURCE_EXT
+        )
+
+        expected_chan_config = ChannelConfig(
+            Channel=1,
+            InputRange=2000,
+            DcOffset=0
+        )
+
+        self.assertEqual(expected_acq_config,self.digitizer.acquisition_config)
+        self.assertEqual(expected_trig_config,self.digitizer.trigger_config)
+        self.assertEqual(expected_chan_config,self.digitizer.channel_config)
+    
+
+    def test_valid_config_file2(self):
+        config_filename = 'tests/example-config-2.toml'
+
+        self.digitizer.load_configuration(config_filename)
+
+        # Create expected config tuples
+        expected_acq_config = AcquisitionConfig(
+            SampleRate=int(500e6),
+            Mode=CS_MODE_SINGLE,
+            SegmentCount=32,
+            SegmentSize=1024,
+            Depth=1024,
+            TriggerDelay=10
+        )
+
+        expected_trig_config = TriggerConfig(
+            Condition=CS_TRIG_COND_NEG_SLOPE,
+            Level=30,
+            Range=2000,
+            Source=CS_TRIG_SOURCE_CHAN_2
+        )
+
+        expected_chan_config = ChannelConfig(
+            Channel=2,
+            InputRange=2000,
+            DcOffset=500
+        )
+
+        self.assertEqual(expected_acq_config,self.digitizer.acquisition_config)
+        self.assertEqual(expected_trig_config,self.digitizer.trigger_config)
+        self.assertEqual(expected_chan_config,self.digitizer.channel_config)
+
+
+    def test_config_file_invalid_trig_condition(self):
+        config_filename = 'tests/invalid-trig-condition-config.toml'
+
+        with self.assertRaises(ValueError):
+            self.digitizer.load_configuration(config_filename)
+
+
+    def test_config_file_invalid_trig_source(self):
+        config_filename = 'tests/invalid-trig-source-config.toml'
+
+        with self.assertRaises(ValueError):
+            self.digitizer.load_configuration(config_filename)
