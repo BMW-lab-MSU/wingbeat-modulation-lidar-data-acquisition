893d0a837d448665b64f86697e31fb408a19248a
Trevor Vannoy <trevorvannoy@montana.edu>
2023-09-26 16:31:14 -0600

    move initialization into setup function

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -10,21 +10,15 @@ class TestDigitizer(unittest.TestCase):
 
     def setUp(self):
         self.digitizer = Digitizer()
+        self.digitizer.initialize()
         # self.mock = MagicMock(spec=PyGage)
     
     def test_free_valid_handle(self):
-        self.digitizer.initialize()
-
-        self.assertEqual(self.digitizer._digitizer_handle,1)
-
         self.digitizer.free()
 
         self.assertEqual(self.digitizer._digitizer_handle,None)
 
     def test_free_invalid_handle(self):
-        self.digitizer.initialize()
-
-        # mock a valid intialization
         self.digitizer._digitizer_handle = 2
 
         self.assertEqual(self.digitizer._digitizer_handle,2)
@@ -33,12 +27,10 @@ class TestDigitizer(unittest.TestCase):
             self.digitizer.free()
     
     def test_double_free(self):
-        self.digitizer.initialize()
-
         self.digitizer.free()
         self.assertEqual(self.digitizer._digitizer_handle,None)
 
         self.digitizer.free()
         self.assertEqual(self.digitizer._digitizer_handle,None)
 
-    
+    # def test_valid_configuration(self):
