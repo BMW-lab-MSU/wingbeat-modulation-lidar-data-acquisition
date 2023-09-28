fa3ebb3f5dd56a611eec5a0383c172b804e7e846
Trevor Vannoy <trevorvannoy@montana.edu>
2023-09-28 11:03:27 -0600

    test trigger delay support
    The trigger delay can only be set in increments of 64.

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -91,6 +91,44 @@ class TestDigitizer(unittest.TestCase):
         self.assertEqual(CS_MODE_SINGLE,actual_acq['Mode'])
         self.assertEqual(actual_acq['Depth'],actual_acq['SegmentSize'])
 
+    def test_valid_configuration_trig_delay(self):
+        config_filename = 'tests/example-config-trig-delay.toml'
+
+        self.digitizer.load_configuration(config_filename)
+
+        self.digitizer.configure()
+
+        # Grab the configuraton dictionaries from the hardware
+        actual_acq = PyGage.GetAcquisitionConfig(self.digitizer._digitizer_handle,CS_ACQUISITION_CONFIGURATION)
+        actual_chan = PyGage.GetChannelConfig(self.digitizer._digitizer_handle,self.digitizer.channel_config.Channel,CS_ACQUISITION_CONFIGURATION)
+        actual_trig = PyGage.GetTriggerConfig(self.digitizer._digitizer_handle,1,CS_ACQUISITION_CONFIGURATION)
+
+        expected_acq = self.digitizer.acquisition_config._asdict()
+        expected_chan = self.digitizer.channel_config._asdict()
+        expected_trig = self.digitizer.trigger_config._asdict()
+
+        # Check that all values we set are as expected
+        for key in expected_trig.keys():
+            self.assertEqual(expected_trig[key],actual_trig[key])
+
+        for key in expected_chan.keys():
+            if key != 'Channel':
+                self.assertEqual(expected_chan[key],actual_chan[key])
+
+        for key in expected_acq.keys():
+            self.assertEqual(expected_acq[key],actual_acq[key])
+        self.assertEqual(CS_MODE_SINGLE,actual_acq['Mode'])
+        self.assertEqual(actual_acq['Depth'],actual_acq['SegmentSize'])
+
+    def test_invalid_trig_delay_config(self):
+        config_filename = 'tests/invalid-trig-delay-config.toml'
+
+        self.digitizer.load_configuration(config_filename)
+
+        with self.assertRaisesRegex(RuntimeError,'Trigger Delay is invalid'):
+            self.digitizer.configure()
+
+
     def test_invalid_trigger_source_config(self):
         config_filename = 'tests/invalid-trig-source-config-2.toml'
 
